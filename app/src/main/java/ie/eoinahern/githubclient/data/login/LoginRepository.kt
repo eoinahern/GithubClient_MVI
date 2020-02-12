8059c26cd1bedde6d1256198794df660bb49a5ce
eoinahern <eoin.ahern@mycit.ie>
2020-02-12 17:30:13 +0000

    added preliminary api repo call to get api key

--- app/src/main/java/ie/eoinahern/githubclient/data/login/LoginRepository.kt
+++ app/src/main/java/ie/eoinahern/githubclient/data/login/LoginRepository.kt
@@ -2,15 +2,41 @@ package ie.eoinahern.githubclient.data.login
 
 import android.content.SharedPreferences
 import ie.eoinahern.githubclient.data.GithubApi
+import ie.eoinahern.githubclient.util.encrypt.EncryptionUtil
+import io.reactivex.Observable
 import javax.inject.Inject
 
 
 class LoginRepository @Inject constructor(
     private val api: GithubApi, private val sharedPreferences: SharedPreferences,
-    private val sharedPrefsEdit: SharedPreferences.Editor
+    private val sharedPrefsEdit: SharedPreferences.Editor,
+    private val encryptionUtil: EncryptionUtil
 ) {
 
+    /**
+     * get user key either from pref
+     *
+     */
+
+    fun getUserKey(): Observable<String> {
+
+        val key = sharedPreferences.getString("", "")
+
+        return if (!key.isNullOrEmpty()) {
+            Observable.just(key)
+        } else {
+            api.getAuthToken("", "", "").map {
+                it.accessToken
+            }.doAfterNext { saveUserKey() }
+        }
+    }
 
 
+    /**
+     * encrypt key and save it to shared prefs!!
+     */
 
+    private fun saveUserKey() {
+
+    }
 }
