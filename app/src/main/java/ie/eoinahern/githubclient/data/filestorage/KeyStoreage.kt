37426960308c4d759f12e6b4caf81c5995cf1267
eoinahern <eoin.ahern@mycit.ie>
2020-04-13 10:45:48 +0100

    decouple key encryption and storage from api call

--- app/src/main/java/ie/eoinahern/githubclient/data/filestorage/KeyStoreage.kt
+++ app/src/main/java/ie/eoinahern/githubclient/data/filestorage/KeyStoreage.kt
@@ -1,35 +1,24 @@
-package ie.eoinahern.githubclient.data.login
+package ie.eoinahern.githubclient.data.filestorage
 
 import android.content.SharedPreferences
 import android.util.Base64
-import android.util.Log
-import androidx.annotation.UiThread
-import ie.eoinahern.githubclient.data.GithubApi
-import ie.eoinahern.githubclient.util.constants.CLIENT_ID
-import ie.eoinahern.githubclient.util.constants.CLIENT_SECRET
 import ie.eoinahern.githubclient.util.constants.GITHUB_TOKEN_KEY
-import ie.eoinahern.githubclient.util.constants.TOKEN_PREFIX
 import ie.eoinahern.githubclient.util.encrypt.EncryptionUtil
 import io.reactivex.Observable
 import java.lang.IllegalArgumentException
 import java.nio.charset.Charset
 import javax.inject.Inject
 
+/**
+ * decouple my file storage of key from the api call
+ *
+ */
 
-class LoginRepository @Inject constructor(
-    private val api: GithubApi,
+class KeyStorage @Inject constructor(
     private val sharedPreferences: SharedPreferences,
     private val sharedPrefsEdit: SharedPreferences.Editor,
     private val encryptionUtil: EncryptionUtil
 ) {
-
-    fun getUserToken(accessCode: String): Observable<String> = api.getAuthToken(
-        CLIENT_ID, CLIENT_SECRET,
-        accessCode, TOKEN_PREFIX.plus(accessCode)
-    ).map {
-        saveUserToken(it.accessToken)
-        it.accessToken
-    }
 
     private fun saveUserToken(token: String) {
         val encryptedToken = encryptionUtil.encrypt(token.toByteArray(Charset.defaultCharset()))
